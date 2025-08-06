59abb01cf4f1cfd33b6e768103049a8905eadec8
nikhilraj021 <dnraj2001@gmail.com>
2025-08-06 21:30:21 +0530

    Refactor homepage to include Navbar component and remove duplicate headers.

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className='w-full fixed flex justify-between items-center px-5 lg:px-8 xl:px-[8%] py-4 z-50 '>
+                <a href="#top">
+                    <h1>Nikhilraj</h1>
+                </a>
+
+                <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3'>
+                    <li>
+                        <a href="#top">Home</a>
+                    </li>
+                    <li>
+                        <a href="#about">About</a>
+                    </li>
+                    <li>
+                        <a href="#services">Services</a>
+                    </li>
+                    <li>
+                        <a href="#work">My Work</a>
+                    </li>
+
+                </ul>
+
+                <div>
+                    <a href="#contact" className='hidden md:block px-10 py-2.5 border border-gray-500 rounded-full '>Contact</a>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
+"use client"
 
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <>
-      <h1 className="text-4xl">Hello World</h1>
-      <h1 className="text-4xl">Hello World</h1>
-      <h1 className="text-4xl">Hello World</h1>
+      <Navbar />
     </>
   );
 }
