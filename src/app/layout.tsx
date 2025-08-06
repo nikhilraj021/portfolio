cf2de3e42fba855b69e82b26caf37ba8689bc7ad
nikhilraj021 <dnraj2001@gmail.com>
2025-08-06 20:15:48 +0530

    Update global styles and layout to include new font families; add multiple Hello World headers to the homepage.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,19 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit, Ovo, Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const outfit = Outfit({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+const ovo = Ovo({
+  weight: ["400"],
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
 });
 
@@ -25,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${outfit.className} ${ovo.className} ${poppins.className} antialiased`}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@
 export default function Home() {
   return (
     <>
+      <h1 className="text-4xl">Hello World</h1>
+      <h1 className="text-4xl">Hello World</h1>
+      <h1 className="text-4xl">Hello World</h1>
     </>
   );
 }
