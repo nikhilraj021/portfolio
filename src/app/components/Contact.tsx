81b8d4370659c8fdd281b1422c3c732d12e8ebf4
nikhilraj021 <dnraj2001@gmail.com>
2025-08-09 14:01:25 +0530

    Add Contact and Work components: implement structure and content for service offerings

---
+++ src/app/components/Contact.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div id="services" className="w-full px-[12%] py-10 scroll-mt-20">
+      <h4 className="text-center mb-2 text-lg font-ovo">What I Offer</h4>
+      <h2 className="text-center text-5xl font-ovo">My Services</h2>
+
+      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo">
+        I am a Full Stack Developer with 2 years of experience with multiple
+        companies like Blackcoat.Ai and SypD Technologies
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
