30b5495ce9cfcd1f3bb7edd2035a0f8a11dc07ce
nikhilraj021 <dnraj2001@gmail.com>
2025-08-15 19:27:28 +0530

    updated

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -8,12 +8,12 @@ import {
   GitBranch,
   BookOpen,
   Rocket,
-  Briefcase,
   Bot,
   Cloud,
   Server,
 } from "lucide-react";
 import { assets } from "../../../assets/assets";
+import Image from "next/image";
 
 export default function Experience() {
   const data = [
@@ -190,28 +190,28 @@ export default function Experience() {
             </div>
           </div>
           <div className="grid grid-cols-2 gap-4">
-            <img
+            <Image
               src={assets.reactjs.src}
               alt="hero template"
               width={500}
               height={500}
               className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
-            <img
+            <Image
               src={assets.nodejs.src}
               alt="feature template"
               width={500}
               height={500}
               className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
-            <img
+            <Image
               src={assets.python.src}
               alt="bento template"
               width={500}
               height={500}
               className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
-            <img
+            <Image
               src={assets.database.src}
               alt="cards template"
               width={500}
@@ -257,7 +257,7 @@ export default function Experience() {
             </div>
           </div>
           <div className="grid grid-cols-2 gap-4">
-            <img
+            <Image
               src={assets.internship.src}
               alt="Internship"
               width={500}
