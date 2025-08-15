44b65df14dfd2dc8bac0af509bfb4e6db7619d0d
nikhilraj021 <dnraj2001@gmail.com>
2025-08-15 12:14:23 +0530

    updated data

--- assets/assets.js
+++ assets/assets.js
@@ -32,6 +32,13 @@ import right_arrow from './right-arrow.png';
 import send_icon from './send-icon.png';
 import right_arrow_bold from './right-arrow-bold.png';
 import right_arrow_bold_dark from './right-arrow-bold-dark.png';
+import intern from './Intern.png';
+import internship from './Internship.png';
+import reactjs from './reactjs.png';
+import nodejs from './nodejs.png';
+import python from './python.png';
+import database from './database.png';
+
 
 export const assets = {
     user_image,
@@ -67,7 +74,13 @@ export const assets = {
     right_arrow,
     send_icon,
     right_arrow_bold,
-    right_arrow_bold_dark
+    right_arrow_bold_dark,
+    intern,
+    internship,
+    reactjs, 
+    nodejs,
+    python,
+    database
 };
 
 
--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { Timeline } from "./ui/timeline";
+import {
+  CheckCircle,
+  Code2,
+  Layers,
+  Database,
+  GitBranch,
+  BookOpen,
+} from "lucide-react";
+import { assets } from "../../../assets/assets";
 
 export default function Experience() {
   const data = [
     {
-      title: "2024",
+      title: "Associate Software Developer",
       content: (
         <div>
           <p className="mb-8 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
@@ -44,41 +53,61 @@ export default function Experience() {
       ),
     },
     {
-      title: "Early 2023",
+      title: "Training Program",
       content: (
         <div>
-          <p className="mb-8 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
-            I usually run out of copy, but when I see content this big, I try to
-            integrate lorem ipsum.
+          <p className="mb-8 text-xs font-semibold text-neutral-800 md:text-sm dark:text-neutral-200">
+            Professional Training in Full Stack Development – NxtWave (Jun 2023
+            – Feb 2024)
           </p>
-          <p className="mb-8 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
-            Lorem ipsum is for people who are too lazy to write copy. But we are
-            not. Here are some more example of beautiful designs I built.
-          </p>
+          <div className="mb-8">
+            <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
+              <BookOpen className="w-4 h-4 text-blue-500" />
+              Completed intensive Full Stack Development training program.
+            </div>
+            <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
+              <Code2 className="w-4 h-4 text-green-500" />
+              Gained expertise in HTML, CSS, Bootstrap, JavaScript, React,
+              Node.js, Express.js, and Python.
+            </div>
+            <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
+              <Layers className="w-4 h-4 text-purple-500" />
+              Built multiple full-stack projects integrating front-end and
+              back-end technologies.
+            </div>
+            <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
+              <Database className="w-4 h-4 text-orange-500" />
+              Worked with SQL databases (SQLite) for data storage and retrieval.
+            </div>
+            <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
+              <GitBranch className="w-4 h-4 text-yellow-500" />
+              Used Git for version control and collaborative development.
+            </div>
+          </div>
           <div className="grid grid-cols-2 gap-4">
             <img
-              src="https://assets.aceternity.com/pro/hero-sections.png"
+              src={assets.reactjs.src}
               alt="hero template"
               width={500}
               height={500}
               className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
             <img
-              src="https://assets.aceternity.com/features-section.png"
+              src={assets.nodejs.src}
               alt="feature template"
               width={500}
               height={500}
               className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
             <img
-              src="https://assets.aceternity.com/pro/bento-grids.png"
+              src={assets.python.src}
               alt="bento template"
               width={500}
               height={500}
               className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
             <img
-              src="https://assets.aceternity.com/cards.png"
+              src={assets.database.src}
               alt="cards template"
               width={500}
               height={500}
@@ -89,54 +118,43 @@ export default function Experience() {
       ),
     },
     {
-      title: "Changelog",
+      title: "Internship",
       content: (
         <div key={"Changelog"}>
           <p className="mb-4 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
-            Deployed 5 new components on Aceternity today
+            Internship – Manac Infotech Pvt. Ltd. (Oct 2022 – Feb 2023)
           </p>
           <div className="mb-8">
             <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
-              ✅ Card grid component
+              <CheckCircle className="w-4 h-4 text-green-500" />
+              Learned HTML, CSS, Bootstrap, and JavaScript for front-end
+              development.
             </div>
             <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
-              ✅ Startup template Aceternity
+              <CheckCircle className="w-4 h-4 text-green-500" />
+              Built responsive and interactive web pages using modern UI
+              frameworks.
             </div>
             <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
-              ✅ Random file upload lol
+              <CheckCircle className="w-4 h-4 text-green-500" />
+              Gained experience in layout design, styling, and basic client-side
+              scripting.
             </div>
             <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
-              ✅ Himesh Reshammiya Music CD
+              <CheckCircle className="w-4 h-4 text-green-500" />
+              Collaborated with team members to understand real-world project
+              workflows.
             </div>
             <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
-              ✅ Salman Bhai Fan Club registrations open
+              <CheckCircle className="w-4 h-4 text-green-500" />
+              Enhanced problem-solving skills by implementing practical web
+              solutions.
             </div>
           </div>
           <div className="grid grid-cols-2 gap-4">
             <img
-              src="https://assets.aceternity.com/pro/hero-sections.png"
-              alt="hero template"
-              width={500}
-              height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
-            />
-            <img
-              src="https://assets.aceternity.com/features-section.png"
-              alt="feature template"
-              width={500}
-              height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
-            />
-            <img
-              src="https://assets.aceternity.com/pro/bento-grids.png"
-              alt="bento template"
-              width={500}
-              height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
-            />
-            <img
-              src="https://assets.aceternity.com/cards.png"
-              alt="cards template"
+              src={assets.internship.src}
+              alt="Internship"
               width={500}
               height={500}
               className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
