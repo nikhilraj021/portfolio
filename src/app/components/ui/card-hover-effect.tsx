bfc9cc5f70da9f7e99f3e8580fa37abecc58ff4a
nikhilraj021 <dnraj2001@gmail.com>
2025-08-15 13:46:09 +0530

    updated

--- src/app/components/ui/card-hover-effect.tsx
+++ src/app/components/ui/card-hover-effect.tsx
@@ -69,7 +69,7 @@ export const Card = ({
   return (
     <div
       className={cn(
-        "rounded-2xl h-full w-full p-4 overflow-hidden bg-black border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
+        "rounded-2xl h-full w-full p-4 overflow-hidden dark:bg-black bg-white border border-black dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
         className
       )}
     >
@@ -87,7 +87,7 @@ export const CardTitle = ({
   children: React.ReactNode;
 }) => {
   return (
-    <h4 className={cn("text-zinc-100 font-bold tracking-wide mt-4", className)}>
+    <h4 className={cn("dark:text-zinc-100 text-zinc-900 font-bold tracking-wide mt-4", className)}>
       {children}
     </h4>
   );
@@ -102,7 +102,7 @@ export const CardDescription = ({
   return (
     <p
       className={cn(
-        "mt-8 text-zinc-400 tracking-wide leading-relaxed text-sm",
+        "mt-8 dark:text-zinc-400 text-zinc-900 tracking-wide leading-relaxed text-sm",
         className
       )}
     >
