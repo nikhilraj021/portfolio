602d74de1205c381f0eb7d5d3ea6e70b3322816e
nikhilraj021 <dnraj2001@gmail.com>
2025-08-15 19:02:56 +0530

    updated effect

--- src/app/components/ui/card-hover-effect.tsx
+++ src/app/components/ui/card-hover-effect.tsx
@@ -27,7 +27,7 @@ export const HoverEffect = ({
     >
       {items.map((item, idx) => (
         <a
-          key={item?.link}
+          key={idx}
           className="relative group  block p-2 h-full w-full"
           onMouseEnter={() => setHoveredIndex(idx)}
           onMouseLeave={() => setHoveredIndex(null)}
